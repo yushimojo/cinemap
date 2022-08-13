2b6a56aba49987e8621917d478eea7984d6f4bac
YuShimojo <yongxiatiao@gmail.com>
2022-08-13 08:06:52 +0000

    会員途中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "f0e33abbc0ae49a08a6394eb0d05a823.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
  
-  namespace :public do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
   # 顧客用
 # URL /customers/sign_in ...
 devise_for :users,skip: [:passwords], controllers: {
@@ -19,4 +13,12 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
+namespace :public do
+    root to: "homes#top"
+  end
+  namespace :admin do
+    root to: "homes#top"
+  end
+  
+end 
