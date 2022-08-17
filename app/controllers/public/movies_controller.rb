4783e5aa361e700346aaca6dba23e182388b27ab
YuShimojo <yongxiatiao@gmail.com>
2022-08-17 15:46:23 +0000

    user途中

--- app/controllers/public/movies_controller.rb
+++ app/controllers/public/movies_controller.rb
@@ -1,2 +1,7 @@
 class Public::MoviesController < ApplicationController
+    
+    def new
+     @movie = Movie.new
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,30 @@
 Rails.application.routes.draw do
  
-  # 顧客用
-# URL /customers/sign_in ...
-devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-namespace :public do
+    # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # 会員用
+  namespace :public do
     root to: "homes#top"
+    resources :movies, only: [:new, :create, :show, :edit, :index, :update]
+      
   end
+    
+  # 管理者用
   namespace :admin do
     root to: "homes#top"
+      
   end
-  
+    
 end 
